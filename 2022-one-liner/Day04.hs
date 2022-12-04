4e22bffd03b0f5c3a43394dc1dceae1cfb849756
Pavel Zwerschke <pavelzw@gmail.com>
2022-12-04 13:37:28 +0100

    liftA2 (||)

--- 2022-one-liner/Day04.hs
+++ 2022-one-liner/Day04.hs
@@ -1,11 +1,12 @@
 import Data.List.Split (splitOn)
 import Data.List (isInfixOf, intersect)
+import Control.Applicative (liftA2)
 
 main :: IO ()
 main = part1 >> part2
 
 part1 :: IO ()
-part1 = print . length . filter (\x -> uncurry (flip isInfixOf) x || uncurry isInfixOf x) . map ((\x -> (head x, x!!1)) . map ((\x -> [(head x)..x!!1]) . map (\x -> read x ::Int) . splitOn "-") . splitOn ",") . lines =<< readFile "Day04.txt"
+part1 = print . length . filter (liftA2 (||) (uncurry (flip isInfixOf)) (uncurry isInfixOf)) . map ((\x -> (head x, x!!1)) . map ((\x -> [(head x)..x!!1]) . map (\x -> read x ::Int) . splitOn "-") . splitOn ",") . lines =<< readFile "Day04.txt"
 
 part2 :: IO ()
 part2 = print . length . filter (not . null) . map (uncurry intersect . (\x -> (head x, x!!1)) . map ((\x -> [(head x)..x!!1]) . map (\x -> read x ::Int) . splitOn "-") . splitOn ",") . lines =<< readFile "Day04.txt"
