c9333ab8eeed90dcd45b5efef07900efe4400ad8
Pavel Zwerschke <pavelzw@gmail.com>
2022-12-09 08:26:59 +0100

    Add Day09 part2

--- 2022/src/Day09.hs
+++ 2022/src/Day09.hs
@@ -4,33 +4,29 @@ import qualified Day
 import Parse (Parser)
 
 import Data.List (nub)
-import Text.Megaparsec (endBy1, (<|>))
+import Text.Megaparsec ((<|>), endBy1)
 import Text.Megaparsec.Char (newline, char, space)
 import Text.Megaparsec.Char.Lexer (decimal)
 
 data Direction = R | D | L | U
-  deriving (Show)
-data Move = Move Direction Int deriving (Show)
-
 type Pos = (Int, Int)
-type HeadTailPos = (Pos, Pos)
+type Rope = [Pos]
 
-type RopePos = [Pos]
+parseInput :: Parser [Direction]
+parseInput = concat <$> endBy1 parseDirections newline
 
-parseInput :: Parser [Move]
-parseInput = endBy1 (Move R <$> (char 'R' *> space *> decimal) <|> Move D <$> (char 'D' *> space *> decimal) <|> Move L <$> (char 'L' *> space *> decimal) <|> Move U <$> (char 'U' *> space *> decimal)) newline
-
-movesToDirections :: [Move] -> [Direction]
-movesToDirections = concat . map (\(Move dir n) -> replicate n dir)
+parseDirections :: Parser [Direction]
+parseDirections = flip replicate <$>
+  (char 'R' *> pure R <|> char 'D' *> pure D <|> char 'L' *> pure L <|> char 'U' *> pure U)
+  <*> (space *> decimal)
 
 distance :: Pos -> Pos -> Int
 -- L_infty distance
 distance (x1, y1) (x2, y2) = max (abs (x1 - x2)) (abs (y1 - y2))
 
-moveTowards :: Pos -> Pos -> Pos
--- moveTowards start end
--- move one step towards end if distance between start and end is > 1
-moveTowards (srcx, srcy) (destx, desty) =
+follow :: Pos -> Pos -> Pos
+-- move one step towards dest if distance between start and end is > 1
+follow (destx, desty) (srcx, srcy) =
   if distance (srcx, srcy) (destx, desty) <= 1
     then (srcx, srcy)
     else (srcx + signum (destx - srcx), srcy + signum (desty - srcy))
@@ -41,19 +37,20 @@ movePos (x, y) D = (x, y - 1)
 movePos (x, y) R = (x + 1, y)
 movePos (x, y) L = (x - 1, y)
 
-move :: HeadTailPos -> Direction -> HeadTailPos
-move (h, t) dir = (h', moveTowards t h')
-  where
-    h' = movePos h dir
+move :: Rope -> Direction -> Rope
+move rope dir = scanl follow (movePos (head rope) dir) (tail rope)
 
-positions :: HeadTailPos -> [Direction] -> [HeadTailPos]
+positions :: Rope -> [Direction] -> [Rope]
 positions = scanl move
 
+numberPositionsTailVisited :: Rope -> [Direction] -> Int
+numberPositionsTailVisited rope = length . nub . map last . positions rope
+
 part1 :: [Direction] -> Int
-part1 = length . nub . map snd . positions ((0,0),(0,0))
+part1 = numberPositionsTailVisited [(0,0),(0,0)]
 
 part2 :: [Direction] -> Int
-part2 = undefined
+part2 = numberPositionsTailVisited (replicate 10 (0,0))
 
-day :: Day.Day [Move] Int
-day = Day.Day parseInput (part1 . movesToDirections) (part2 . movesToDirections)
+day :: Day.Day [Direction] Int
+day = Day.Day parseInput part1 part2
