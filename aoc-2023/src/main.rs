0491cd7dec474430733390f24ddd3b47959b465e
Pavel Zwerschke <pavelzw@gmail.com>
2023-12-01 15:51:39 +0100

    Day01

---
+++ aoc-2023/src/main.rs
@@ -0,0 +1,83 @@
+mod y2023;
+
+use std::os::unix::prelude::OsStrExt;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+use std::{fmt, fs, io};
+
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(required = true)]
+    files: Vec<PathBuf>,
+}
+
+macro_rules! days {
+    ( $( $day:ident : $name:expr => $path:path, )* ) => {
+        enum Day { $( $day, )* }
+
+        impl fmt::Display for Day {
+            fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Result {
+                match self {
+                    $( Self::$day => $name, )*
+                }.fmt(f)
+            }
+        }
+
+        impl FromStr for Day {
+            type Err = ();
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                Ok(match s {
+                    $( $name => Self::$day, )*
+                    _ => return Err(()),
+                })
+            }
+        }
+
+        impl Day {
+            fn solve(self, input: String) {
+                match self {
+                    $( Self::$day => $path(input), )*
+                }
+            }
+        }
+    };
+}
+
+impl Day {
+    fn from_path(path: &Path) -> Option<Self> {
+        let file_name = path.file_stem()?.as_bytes();
+        let file_name = std::str::from_utf8(file_name).ok()?;
+        let day = file_name.strip_prefix("Day")?;
+        let day = day.parse().ok()?;
+        Some(day)
+    }
+}
+
+days! {
+    Y2023D01: "01" => y2023::d01::solve,
+}
+
+
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+
+    for file in args.files {
+        let day = match Day::from_path(&file) {
+            Some(day) => day,
+            None => {
+                eprintln!("Could not parse day from file {:?}", file);
+                continue;
+            }
+        };
+
+        eprintln!("Solving day {}", day);
+        let input = fs::read_to_string(file)?;
+        day.solve(input);
+        eprintln!();
+    }
+    Ok(())
+}
---
+++ aoc-2023/src/y2023/d01.rs
@@ -0,0 +1,52 @@
+macro_rules! map_string_to_number {
+    ($s:expr) => {
+        match $s {
+            "one" | "1" => 1,
+            "two" | "2" => 2,
+            "three" | "3" => 3,
+            "four" | "4" => 4,
+            "five" | "5" => 5,
+            "six" | "6" => 6,
+            "seven" | "7" => 7,
+            "eight" | "8" => 8,
+            "nine" | "9" => 9,
+            _ => panic!("{} not found", $s),
+        }
+    };
+}
+
+pub fn solve(input: String) {
+    let sum_level_1: i32 = input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            let first = line.chars().find(|c| c.is_numeric()).unwrap();
+            let last = line.chars().rev().find(|c| c.is_numeric()).unwrap();
+            format!("{}{}", first, last).parse::<i32>().unwrap()
+        })
+        .sum();
+
+    let sum_level_2: i32 = input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            let numbers = [
+                "one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9"
+            ];
+            let positions = numbers
+                .iter()
+                .map(|n| (line.find(n), line.rfind(n)))
+                .enumerate()
+                .filter(|(_, (i, _))| i.is_some())
+                .map(|(index, (find, rfind))| (index, find.unwrap(), rfind.unwrap()));
+            let first = positions.clone().min_by_key(|(_, i, _)| *i).unwrap().0;
+            let first = map_string_to_number!(numbers[first]);
+            let last = positions.max_by_key(|(_, _, i)| *i).unwrap().0;
+            let last = map_string_to_number!(numbers[last]);
+            first * 10 + last
+        })
+        .sum();
+
+    println!("Level 1: {}", sum_level_1);
+    println!("Level 2: {}", sum_level_2);
+}
